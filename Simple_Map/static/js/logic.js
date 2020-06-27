e37d0858a29f0ae8011be7833b5c183c1a06e801
keyoumao <58833084+keyoumao@users.noreply.github.com>
2020-06-27 12:26:57 -0400

    add .gitignore file

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -8,7 +8,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-  id: 'mapbox/streets-v11',
+	id: 'mapbox/streets-v11',
 	accessToken: API_KEY
 })
 
